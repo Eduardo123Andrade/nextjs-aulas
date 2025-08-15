f2d28e3bc109b2af838e14f25dbe2c815293bd67
Eduardo Andrade <eduardo.andrade15@hotmail.com>
2025-08-15 18:04:49 +0000

    create button compoent

--- site-blog/src/pages/index.tsx
+++ site-blog/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Button } from "@/components/ui/button";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,6 +16,8 @@ export default function Home() {
   return (
     <div className="bg-black grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h2 className="text-4xl text-red-600">Hello world</h2>
+
+      <Button variant="secondary">Button</Button>
     </div>
   );
 }
