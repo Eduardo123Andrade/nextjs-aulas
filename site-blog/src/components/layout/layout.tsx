1e0e0e8c90bd9d128580500fa3cbaefe6dd4af16
Eduardo Andrade <eduardo.andrade15@hotmail.com>
2025-08-16 18:28:57 +0000

    feat: create laytou

---
+++ site-blog/src/components/layout/layout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Header } from "../header";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="relative flex min-h-screen flex-col dark">
+      <Header />
+      <main className="flex-1 flex flex-col mb-12">{children}</main>
+    </div>
+  );
+};
