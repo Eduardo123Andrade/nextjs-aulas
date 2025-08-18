9cccc8044b0be4000feced74fd5f1806022ade3f
Eduardo Andrade <eduardo.andrade15@hotmail.com>
2025-08-18 14:45:37 +0000

    fix style guide on footer

--- site-blog/src/components/footer/footer.tsx
+++ site-blog/src/components/footer/footer.tsx
@@ -5,21 +5,22 @@ import { Logo } from "../logo";
 
 export const Footer = () => {
   return (
-    <footer className="border-top border-white/10 bg-black">
+    <footer className="bg-gray-500">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between md:flex-row gap-8 py-8">
           <Logo />
-          <nav className="flex flex-col md:flex-row itens-centar gap-4 text-sm text-muted-foreground">
-            <Link className="hover:text-primary" href="/termos-de-uso">
+
+          <nav className="flex flex-col md:flex-row itens-centar gap-4 text-sm">
+            <Link className="hover:text-blue-200" href="/termos-de-uso">
               Termos de uso
             </Link>
             <Link
-              className="hover:text-primary"
+              className="hover:text-blue-200"
               href="/politica-de-privacidade"
             >
               Política de privacidade
             </Link>
-            <Link className="hover:text-primary" href="/enviar-feedback">
+            <Link className="hover:text-blue-200" href="/enviar-feedback">
               Enviar feedback
             </Link>
           </nav>
