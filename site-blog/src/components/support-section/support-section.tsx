629f893e93e516906249755105afef0c57d3fed9
Eduardo Andrade <eduardo.andrade15@hotmail.com>
2025-08-18 18:35:04 +0000

    componentize support card

--- site-blog/src/components/support-section/support-section.tsx
+++ site-blog/src/components/support-section/support-section.tsx
@@ -1,5 +1,6 @@
 import { HeartHandshake, PaintbrushVertical, Store } from "lucide-react";
 import { PT_Sans_Caption } from "next/font/google";
+import { SupportCard } from "./support-card";
 
 const ptSansCaption = PT_Sans_Caption({ subsets: ["latin"], weight: "700" });
 
@@ -15,49 +16,27 @@ export const SupportSection = () => {
 
         <div className="grid gap-6 md:grid-cols-3">
           {/** Card 1 */}
-          <div className="flex flex-col text-left gap-2 rounded-lg p-6 md:p-12 bg-blue-400">
-            <div className="flex h-12 w-12 items-center justify-center rounded-lg bg-blue-300 mb-4">
-              <PaintbrushVertical className="h-6 w-6 text-white" />
-            </div>
-
-            <strong className="text-heading-sm text-gray-100">
-              Personalize seu site
-            </strong>
-            <p className="text-body-sm text-gray-200">
-              Adicione sua logo, favicon, cores no seu catalago e tenha tudo com
-              a sua cara.
-            </p>
-          </div>
+          <SupportCard
+            title="Personalize seu site"
+            description="Adicione sua logo, favicon, cores no seu catalago e tenha tudo com a sua cara."
+            Illustration={PaintbrushVertical}
+          />
 
           {/** Card 2 */}
-          <div className="flex flex-col text-left gap-2 rounded-lg p-6 md:p-12 bg-cyan-300">
-            <div className="flex h-12 w-12 items-center justify-center rounded-lg bg-cyan-200 mb-4">
-              <Store className="h-6 w-6 text-white" />
-            </div>
-
-            <strong className="text-heading-sm text-gray-100">
-              Venda de qualquer loja
-            </strong>
-            <p className="text-body-sm text-gray-200">
-              Não importa a loja, o Site.Set permite que você insera qualquer
-              link de afiliado.
-            </p>
-          </div>
+          <SupportCard
+            title="Venda de qualquer loja"
+            description="Não importa a loja, o Site.Set permite que você insera qualquer link de afiliado."
+            Illustration={Store}
+            backgroundColor="bg-cyan-300"
+            iconBackgroundColor="bg-cyan-200 mb-4"
+          />
 
           {/** Card 3 */}
-          <div className="flex flex-col text-left gap-2 rounded-lg p-6 md:p-12 bg-blue-400">
-            <div className="flex h-12 w-12 items-center justify-center rounded-lg bg-blue-300 mb-4">
-              <HeartHandshake className="h-6 w-6 text-white" />
-            </div>
-
-            <strong className="text-heading-sm text-gray-100">
-              Receba suporte amigável
-            </strong>
-            <p className="text-body-sm text-gray-200">
-              Nossa equipe estará sempre pronta para te atender para ajudar no
-              que for preciso.
-            </p>
-          </div>
+          <SupportCard
+            title="Receba suporte amigável"
+            description="Nossa equipe estará sempre pronta para te atender para ajudar no que for preciso"
+            Illustration={HeartHandshake}
+          />
         </div>
       </div>
     </section>
