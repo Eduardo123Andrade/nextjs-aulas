9b5b726f4d54395dd627d81b9e391f209999798c
Eduardo Andrade <eduardo.andrade15@hotmail.com>
2025-08-18 14:45:52 +0000

    fix style guide on layout

--- site-blog/src/components/layout/layout.tsx
+++ site-blog/src/components/layout/layout.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { Header } from "../header";
 import { Footer } from "../footer";
+import { Inter } from "next/font/google";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
+const inter = Inter({ subsets: ["latin"] });
+
 export const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="relative flex min-h-screen flex-col dark">
+    <div
+      className={`${inter.className} relative flex min-h-screen flex-col dark`}
+    >
       <Header />
       <main className="flex-1 flex flex-col pb-12 bg-black">{children}</main>
       <Footer />
