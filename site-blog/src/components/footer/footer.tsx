52be13ef19a7dfb917c3498237e48e277ec361b5
Eduardo Andrade <eduardo.andrade15@hotmail.com>
2025-08-18 13:54:29 +0000

    use logo component on header and footer

--- site-blog/src/components/footer/footer.tsx
+++ site-blog/src/components/footer/footer.tsx
@@ -1,15 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 import { ActiveLink } from "../active-link";
+import { Logo } from "../logo";
 
 export const Footer = () => {
   return (
     <footer className="border-top border-white/10 bg-black">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between md:flex-row gap-8 py-8">
-          <Link href="">
-            <Image src="/Logo.svg" alt="Logo site" width={116} height={32} />
-          </Link>
+          <Logo />
           <nav className="flex flex-col md:flex-row itens-centar gap-4 text-sm text-muted-foreground">
             <Link className="hover:text-primary" href="/termos-de-uso">
               Termos de uso
