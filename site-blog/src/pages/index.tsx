9cfc0689c9c0561e58772d1948e9ce3ed363bf4e
Eduardo Andrade <eduardo.andrade15@hotmail.com>
2025-08-18 15:23:23 +0000

    create hero section

--- site-blog/src/pages/index.tsx
+++ site-blog/src/pages/index.tsx
@@ -1,23 +1,11 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-import { Button } from "@/components/ui/button";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { HeroSection } from "@/components/hero-section";
 
 export default function Home() {
   return (
-    <div className="bg-black grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h2 className="text-4xl text-red-600">Hello world</h2>
-
-      <Button variant="secondary">Button</Button>
-    </div>
+    <>
+      <article className="flex flex-col">
+        <HeroSection />
+      </article>
+    </>
   );
 }
