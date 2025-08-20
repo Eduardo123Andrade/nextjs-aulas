fe68d947de28ed56705ed0aeb85cea1457320617
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2025-08-20 16:21:50 -0300

    create second version to search

--- site-blog/src/components/search/search.tsx
+++ site-blog/src/components/search/search.tsx
@@ -1,33 +1,46 @@
+import { cn } from "@/lib/utils";
+import { isStringEmpty } from "@/utils";
 import { SearchIcon } from "lucide-react";
 import { useRouter } from "next/router";
 import { useCallback, useEffect, useState } from "react";
 
 export const Search = () => {
   const router = useRouter();
-  const q = router.query.q as string;
-  const [query, setQuery] = useState("");
+  const query = router.query.q as string;
 
   const handleSearch = useCallback(
     (event: React.FormEvent) => {
       event.preventDefault();
 
       if (query.trim()) {
-        router.push(`/blog?q=${query}`);
+        router.push(`/blog?q=${encodeURIComponent(query)}`);
       }
     },
     [query, router]
   );
 
-  useEffect(() => {
-    setQuery(q);
-  }, []);
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newQuery = event.target.value;
+
+    const config = {
+      shallow: true,
+      scroll: false,
+    };
+
+    if (isStringEmpty(newQuery)) {
+      return router.push("/blog", undefined, config);
+    }
+    router.push(`/blog?q=${encodeURIComponent(newQuery)}`, undefined, config);
+  };
+
+  const className = cn(
+    "text-gray-300 absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 transition-colors duration-200 group-focus-within:text-blue-300",
+    query ? "text-blue-300" : ""
+  );
 
   return (
-    <form className="relative" onSubmit={handleSearch}>
-      <SearchIcon
-        className="text-gray-300 absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 
-      transition-colors duration-200"
-      />
+    <form className="relative group" onSubmit={handleSearch}>
+      <SearchIcon className={className} />
       <input
         type="text"
         placeholder="Buscar"
@@ -35,7 +48,7 @@ export const Search = () => {
         rounded-md text-body-sm outline-none transition-all duration-200 focus-within:border-blue-300
         focus-within:right-1 focus-within:ring-blue-300 placeholder:text-gray-300
         placeholder:text-body-sm"
-        onChange={(event) => setQuery(event.target.value)}
+        onChange={handleQueryChange}
       />
     </form>
   );
