edea4ce786cbde928cfa052575f2a3f6de13f6c3
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2025-08-21 16:30:38 -0300

    use post card and post grid card components on blog list

--- site-blog/src/templates/blog/blog-list.tsx
+++ site-blog/src/templates/blog/blog-list.tsx
@@ -1,5 +1,7 @@
 import { Search } from "@/components/search";
 import { useRouter } from "next/router";
+import { PostCard } from "./components/post-card";
+import { PostGridCard } from "./components";
 
 const PAGE_TITLE = "Dicas e estratégias para impulsionar seu negócio";
 
@@ -10,9 +12,9 @@ export const BlogList = () => {
   const pageTitle = query ? `Resultados de busca para: ${query}` : PAGE_TITLE;
 
   return (
-    <div className="flex flex-col py-24 flex-grow h-full">
-      <header className="">
-        <div className="container space-y-6 flex flex-col items-start justify-between md:flex-row md:items-end lg:items-end">
+    <div className="container flex flex-col py-24 flex-grow h-full">
+      <header className="pb-14">
+        <div className="space-y-6 flex flex-col items-start justify-between md:flex-row md:items-end lg:items-end">
           <div className="flex flex-col gap-4 px-4 md:px-0">
             <span className="text-cyan-100 text-body-tag w-fit rounded-md text-center md:text-left py-2 px-4 bg-cyan-300">
               BLOG
@@ -28,6 +30,20 @@ export const BlogList = () => {
           <Search />
         </div>
       </header>
+
+      <PostGridCard>
+        <PostCard
+          title="Transformando seu negócio em uma loja virtual"
+          description="Se você está buscando uma maneira simples e eficaz de vender seus produtos online, o Site..."
+          date="20/12/24"
+          image="/assets/primeiro-post.png"
+          slug=""
+          author={{
+            avatar: "/customer-01.png",
+            name: "Aspen Dokidis",
+          }}
+        />
+      </PostGridCard>
     </div>
   );
 };
