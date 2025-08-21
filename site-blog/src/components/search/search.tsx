df1e46fc0d939539f79543fecb0a05fb21784689
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2025-08-21 11:27:32 -0300

    add clear icon on search component

--- site-blog/src/components/search/search.tsx
+++ site-blog/src/components/search/search.tsx
@@ -1,12 +1,12 @@
 import { cn } from "@/lib/utils";
 import { isStringEmpty } from "@/utils";
-import { SearchIcon } from "lucide-react";
+import { CircleX, SearchIcon } from "lucide-react";
 import { useRouter } from "next/router";
 import { useCallback, useEffect, useState } from "react";
 
 export const Search = () => {
   const router = useRouter();
-  const query = router.query.q as string;
+  const query = (router.query.q as string) ?? "";
 
   const handleSearch = useCallback(
     (event: React.FormEvent) => {
@@ -33,23 +33,38 @@ export const Search = () => {
     router.push(`/blog?q=${encodeURIComponent(newQuery)}`, undefined, config);
   };
 
+  const resetSearch = () => {
+    router.push("/blog", undefined, { shallow: true, scroll: false });
+  };
+
   const className = cn(
     "text-gray-300 absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 transition-colors duration-200 group-focus-within:text-blue-300",
     query ? "text-blue-300" : ""
   );
 
   return (
-    <form className="relative group" onSubmit={handleSearch}>
+    <form className="relative group w-full md:w-60" onSubmit={handleSearch}>
       <SearchIcon className={className} />
       <input
         type="text"
         placeholder="Buscar"
-        className="h-10 w-72 bg-transparent border border-gray-400 pl-9 text-gray-100
+        value={query}
+        className="w-full h-10 md:w-60 bg-transparent border border-gray-400 pl-9 text-gray-100
         rounded-md text-body-sm outline-none transition-all duration-200 focus-within:border-blue-300
         focus-within:right-1 focus-within:ring-blue-300 placeholder:text-gray-300
         placeholder:text-body-sm"
         onChange={handleQueryChange}
       />
+
+      {!!query && (
+        <CircleX
+          onClick={resetSearch}
+          className="
+        text-gray-300 absolute right-3 top-1/2 -translate-y-1/2 h-4 w-4
+        transition-colors duration-200 group-focus-within:text-blue-300
+        cursor-pointer"
+        />
+      )}
     </form>
   );
 };
