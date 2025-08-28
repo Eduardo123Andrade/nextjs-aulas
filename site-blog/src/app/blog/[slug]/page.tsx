8da256af8e6d30d4fb2fbe38ee2d182ac8a83673
Eduardo Andrade <eduardo.andrade15@hotmail.com>
2025-08-28 14:06:32 -0300

    generate automatic metadata

--- site-blog/src/app/blog/[slug]/page.tsx
+++ site-blog/src/app/blog/[slug]/page.tsx
@@ -1,10 +1,32 @@
 import PostPage from "@/templates/blog/post-page";
+import { METADATA } from "@/utils";
 import { allPosts } from "contentlayer/generated";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
+import { title } from "process";
 
 interface BlogPostPageProps {
   params: Promise<{ slug: string }>;
 }
+
+export const generateMetadata = async ({
+  params,
+}: BlogPostPageProps): Promise<Metadata> => {
+  const { slug } = await params;
+  const post = allPosts.find((item) => item.slug.toLocaleLowerCase() === slug);
+  if (!post) return {};
+
+  return {
+    ...METADATA,
+    title: post.title,
+    description: post.description,
+    authors: [{ name: post.author.name }],
+    openGraph: {
+      ...METADATA.openGraph,
+      images: [post.image],
+    },
+  };
+};
 
 export default async function BlogPostPage({ params }: BlogPostPageProps) {
   const { slug } = await params;
