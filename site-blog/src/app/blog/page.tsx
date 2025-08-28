43717d365e77dc6c71799bbfcad3313c6c6e4ae6
Eduardo Andrade <eduardo.andrade15@hotmail.com>
2025-08-28 13:51:18 -0300

    add metadata on layout and blog page

--- site-blog/src/app/blog/page.tsx
+++ site-blog/src/app/blog/page.tsx
@@ -1,5 +1,18 @@
 import { BlogList } from "@/templates/blog";
+import { METADATA } from "@/utils";
 import { allPosts } from "contentlayer/generated";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  ...METADATA,
+  title: "Blog",
+  description: "Dicas e estratégias para impulsionar seu negócio",
+  openGraph: {
+    ...METADATA.openGraph,
+    title: "Blog",
+    description: "Dicas e estratégias para impulsionar seu negócio",
+  },
+};
 
 export default function BlogPage() {
   const sortedPosts = allPosts.sort(
